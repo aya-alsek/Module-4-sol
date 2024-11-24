1084a6a8d6fcd32496f4c8af5c3faec9ac86d152
aya-alsek <139685108+aya-alsek@users.noreply.github.com>
2024-11-24 11:12:44 +0200

    Add files via upload

---
+++ SpeakHello.js
@@ -0,0 +1,10 @@
+var helloSpeaker = (function () {
+    var speakWord = "Hello";
+  
+    return {
+      speak: function (name) {
+        console.log(speakWord + " " + name);
+      }
+    };
+  })();
+  
